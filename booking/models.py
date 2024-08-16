6c57e480e3486142c1a324e896cbee446d92a89e
Ngocdzung Smith <hysinh1@gmail.com>
2024-08-16 14:48:27 +0000

    Feat: Adds Booking model and adds to admin panel

--- booking/models.py
+++ booking/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+TYPE_OF_EVENT_CHOICES = {
+        ("WED", "Wedding"),
+        ("COR", "Corporate Event"),
+        ("GAL", "Gala Event"),
+        ("OCC", "Special Occasion"),
+        ("WSP", "Workshop"),
+        ("OTH", "Other"),
+    }
+
+STATUS = ((0, "Pending"), (1, "Approved"))
+
+
 # Create your models here.
 class Venue(models.Model):
     """
@@ -9,9 +22,32 @@ class Venue(models.Model):
     venue_id = models.IntegerField(unique=True)
     venue_name = models.CharField(max_length=100)
     staff_member = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name="venue_bookings"
+        User, on_delete=models.PROTECT, related_name="venue_listings"
     )
     venue_description = models.TextField()
     venue_blurb = models.TextField(blank=True)
     venue_capacity = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+# TextChoice code source:
+# https://docs.djangoproject.com/en/5.0/ref/models/fields/#enumeration-types
+class Booking(models.Model):
+    """
+    Stores a single booking entry
+    """
+    booking_id = models.IntegerField(unique=True)
+    venue = models.ForeignKey(Venue, on_delete=models.PROTECT)
+    client_id = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="venue_bookings"
+        )
+    event_type = models.CharField(
+        max_length=3,
+        choices=TYPE_OF_EVENT_CHOICES
+    )
+    event_date = models.DateField()
+    num_guests = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+

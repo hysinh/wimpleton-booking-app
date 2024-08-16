f5995138b3a6135a928460e339e27ce124107e2e
Ngocdzung Smith <hysinh1@gmail.com>
2024-08-16 13:19:47 +0000

    Feat: Adds Venue model to admin panel

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Venue(models.Model):
+    """
+    Stores a single venue entry 
+    """
+    venue_id = models.IntegerField(unique=True)
+    venue_name = models.CharField(max_length=100)
+    staff_member = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="venue_bookings"
+    )
+    venue_description = models.TextField()
+    venue_blurb = models.TextField(blank=True)
+    venue_capacity = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
--- wimpleton/settings.py
+++ wimpleton/settings.py
@@ -27,9 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
+    ]
 
 
 # Application definition
@@ -86,6 +90,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "http://127.0.0.1:8000",
+    "https://*.ws.codeinstitute-ide.net",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

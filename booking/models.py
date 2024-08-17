a98b78fb4056ccf4850688a0d4c43c5f31b48587
Ngocdzung Smith <hysinh1@gmail.com>
2024-08-17 10:04:16 +0000

    Adds unique validation for booking date and venue

--- booking/models.py
+++ booking/models.py
@@ -19,7 +19,6 @@ class Venue(models.Model):
     """
     Stores a single venue entry 
     """
-    venue_id = models.IntegerField(unique=True)
     venue_name = models.CharField(max_length=100)
     staff_member = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name="venue_listings"
@@ -36,7 +35,7 @@ class Booking(models.Model):
     """
     Stores a single booking entry
     """
-    booking_id = models.IntegerField(unique=True)
+    booking_id = models.AutoField(primary_key=True, default="001")
     venue = models.ForeignKey(Venue, on_delete=models.PROTECT)
     client_id = models.ForeignKey(
         User, on_delete=models.PROTECT, related_name="venue_bookings"
@@ -50,4 +49,7 @@ class Booking(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('venue', 'event_date')
 

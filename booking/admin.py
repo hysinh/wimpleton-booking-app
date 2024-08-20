f1ec143831cd0fc7ea774a45f3e3fef91377fb6b
Ngocdzung Smith <hysinh1@gmail.com>
2024-08-20 13:46:30 +0000

    Install django-summernote

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Venue, Booking
 
 # Register your models here.
-admin.site.register(Venue)
+@admin.register(Venue)
+class VenueAdmin(SummernoteModelAdmin):
+    list_display = ('venue_name', 'venue_blurb' )
+    summernote_fields = ('venue_description')
+
+
 admin.site.register(Booking)

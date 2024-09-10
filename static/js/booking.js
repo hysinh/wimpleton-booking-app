115f1cb3de0229a58eacb98657bd97ddd9d7bb80
Ngocdzung Smith <hysinh1@gmail.com>
2024-09-10 17:31:32 +0000

    Feat: Add Delete Booking functionality

--- static/js/booking.js
+++ static/js/booking.js
@@ -17,7 +17,7 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
     let bookingId = e.target.getAttribute("data-booking_id");
-    deleteConfirm.href = `delete_comment/${bookingId}`;
+    deleteConfirm.href = `../delete-booking/${bookingId}`;
     deleteModal.show();
   });
 }

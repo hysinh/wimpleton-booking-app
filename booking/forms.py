94eade5db412061767b4a8bcfd80c57e8c1ebc65
Ngocdzung Smith <hysinh1@gmail.com>
2024-09-24 12:41:28 +0000

    Update forms test with data validation tests for email and booking forms. Booking form not working

--- booking/forms.py
+++ booking/forms.py
@@ -5,21 +5,21 @@ from datetime import timedelta
 from django.core.exceptions import NON_FIELD_ERRORS
 from .models import Booking, Email
 
-TYPE_OF_EVENT_CHOICES = {
-        ("WED", "Wedding"),
-        ("COR", "Corporate Event"),
-        ("GAL", "Gala Event"),
-        ("OCC", "Special Occasion"),
-        ("WSP", "Workshop"),
-        ("OTH", "Other"),
-    }
+# TYPE_OF_EVENT_CHOICES = {
+#         ("WED", "Wedding"),
+#         ("COR", "Corporate Event"),
+#         ("GAL", "Gala Event"),
+#         ("OCC", "Special Occasion"),
+#         ("WSP", "Workshop"),
+#         ("OTH", "Other"),
+#     }
 
-VENUES = {
-        ("The Chapel"),
-        ("The Mansion Formal Rooms"),
-        ("The Conservatory"),
-        ("OCC"),
-    }
+# VENUES = {
+#         ("The Chapel"),
+#         ("The Mansion Formal Rooms"),
+#         ("The Conservatory"),
+#         ("OCC"),
+#     }
 
 # DateInput Source: 
 # https://stackoverflow.com/questions/74227268/how-to-make-a-date-picker-that-does-not-select-previous-dates-in-django
--- booking/test_forms.py
+++ booking/test_forms.py
@@ -1,14 +1,39 @@
 from django.test import TestCase
+from .models import Booking, Email
 from .forms import BookingForm, EmailForm
 
 # Create your tests here.
-class TestBookingForm(TestCase):
+class TestEmailForm(TestCase):
+    """
+    Test cases for the email form
+    """
+    def test_email_form_is_valid(self):
+        """
+        Test the Email contact form with valid data
+        """
+        data = {
+            "name": 'Ralph Test',
+            "email": "test@test.com",
+            "message": "Hello, This is a test",
+        }
+        form = EmailForm(data=data)
+        self.assertTrue(form.is_valid())
 
-    def test_form_is_valid(self):
-        booking_form = BookingForm(
-            {'venue': '1'},
-            {'event_type': 'Wedding'},
-            {'event_date': '2024-10-20'},
-            {'num_guests': '250'}
-        )
-        self.assertTrue(booking_form.is_valid())
+
+class TestBookingForm(TestCase):
+    """
+    Test cases for the Booking form
+    """
+    def test_booking_form_is_valid(self):
+        """
+        Test the Booking form with valid data
+        """
+        data = {
+            "venue": 1,
+            "event_type": "Gala",
+            "event_date": "2024-09-18",
+            "num_guests": "200",
+            "client": 1,
+        }
+        form = BookingForm(data=data)
+        self.assertTrue(form.is_valid()) 
